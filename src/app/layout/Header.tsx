2a3fc2bf0690cd13bd5f5a47a6995ced2ad33ef0
eraldalikcani <eralda.likcani@fshnstudent.info>
2023-02-20 23:52:24 +0100

    first commit

---
+++ src/app/layout/Header.tsx
@@ -0,0 +1,66 @@
+import { AppBar, Box, List, ListItem, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+    { title: 'Pets', path: '/Pets' }
+]
+
+const rightLinks = [
+    { title: 'login', path: '/login' },
+    { title: 'register', path: '/register' },
+
+]
+
+const navStyles = {
+    color: 'inherit',
+    textDecoration: 'none',
+    typography: 'h6',
+    '&:hover': {
+        color: 'grey.500'
+    },
+    '&.active': {
+        color: 'text.secondary'
+    }
+}
+
+export default function Header() {
+    return (
+        <AppBar position="static" sx={{ mb: 4 }}>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Box display='flex' alignItems='center'>
+                <Typography gutterBottom color='secondary' variant="h5">
+                    PetStore
+                </Typography>
+                </Box>
+                
+                <List sx={{ display: 'flex' }}>
+                    {midLinks.map(({ title, path }) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={navStyles}
+                        >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+
+                <Box display='flex' alignItems='center'>
+                    <List sx={{ display: 'flex' }}>
+                        {rightLinks.map(({ title, path }) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyles}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+}
